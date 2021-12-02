dbd79acbf1017cef7913b2df4b497b8ba5d1538d
Daniel <danielufeli@yahoo.com>
2021-12-02 15:10:44 +0100

    css and html

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+import './style.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+
+function component() {
+  const element = document.createElement('div');
+
+  //Lodash, now imported by this script
+
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
