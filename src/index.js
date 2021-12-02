73fc5256645ff4fffaeea7e2564dfd007dcb3481
Daniel <danielufeli@yahoo.com>
2021-12-02 15:43:43 +0100

    Add javascript array and linters

--- src/index.js
+++ src/index.js
@@ -6,13 +6,48 @@ import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 
 function component() {
-  const element = document.createElement('div');
+  let data = [
+    {
+      index: 1,
+      description: 'Buy a Laptop',
+      completed: false,
+    },
+    {
+      index: 2,
+      description: 'Attend AOT LAgos Event',
+      completed: false,
+    },
+    {
+      index: 3,
+      description: 'Work on Javascript',
+      completed: false,
+    },
+  ];
+
+  // const task = document.querySelector('.task').innerHTML;
+  let task = '';
+
+  if (data.length === 0) {
+    return (document.querySelector('#task').innerHTML =
+      '<p>No Record Found</p>');
+  } else {
+    return data.map((e, i) => {
+      task += `<li class="task">
+          <input type="checkbox" id="task-${e.index}" />
+          <input type="text" value="${e.description}" />
+          <span>
+            <i class="fas fa-ellipsis-v"></i>
+          </span>
+        </li>`;
+      return (document.querySelector('#task').innerHTML = task);
+    });
+  }
 
   //Lodash, now imported by this script
 
   // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
+  // return element;
 }
 
 document.body.appendChild(component());
