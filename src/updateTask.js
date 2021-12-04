c0015152c3352bf7c827e395f3c6380fff1435f4
Daniel <danielufeli@yahoo.com>
2021-12-04 10:28:55 +0100

    Add filter for completed task

--- src/updateTask.js
+++ src/updateTask.js
@@ -1,16 +1,11 @@
-const updateTask = (event) => {
-  // const todos = JSON.parse(localStorage.getItem('todos'));
-  // todos.forEach((todo) => {
-  //   if (todo.index.toString() === index) {
-  //     if (todo.completed === true) {
-  //       todo.completed = false;
-  //     } else {
-  //       todo.completed = true;
-  //     }
-  //   }
-  //   return todo;
-  // });
-  // localStorage.setItem('todos', JSON.stringify(todos));
-  console.log(event);
+const updateTask = (task, index) => {
+  const todos = JSON.parse(localStorage.getItem('todos'));
+  todos.forEach((todo) => {
+    if (todo.index.toString() === index) {
+      todo.description = task;
+    }
+    return todo;
+  });
+  localStorage.setItem('todos', JSON.stringify(todos));
 };
 export default updateTask;
